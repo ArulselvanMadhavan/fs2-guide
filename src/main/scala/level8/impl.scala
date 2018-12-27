9eea4618a23f3446252469c2a2e74b2f9bb88531
Arul Madhavan <arulselvan1234@gmail.com>
2018-12-27 13:28:26 -0800

    Finished Chapter 8

--- src/main/scala/level8/impl.scala
+++ src/main/scala/level8/impl.scala
@@ -10,8 +10,17 @@ object impl {
     */
   def evalMapAccumulate[F[_], S, O, O2](s1: Stream[F, O])(init: S)(
       f: (S, O) => F[(S, O2)]): Stream[F, (S, O2)] = {
-    def go(s: S, in: Stream[F, O]): Pull[F, (S, O2), Unit] =
-      ???
+    def go(s: S, in: Stream[F, O]): Pull[F, (S, O2), Unit] = {
+      in.pull.uncons1.flatMap[F, (S, O2), Unit] {
+        case None => Pull.done
+        case Some((o, tl)) => {
+          Pull.eval(f(s, o)).flatMap {
+            case (nextState, o2) =>
+              Pull.output1((nextState, o2)) >> go(nextState, tl)
+          }
+        }
+      }
+    }
 
     go(init, s1).stream
   }
@@ -19,5 +28,13 @@ object impl {
   /**
     * Emits only inputs which match the supplied predicate
     */
-  def filter[F[_], O](s1: Stream[F, O])(f: O => Boolean): Stream[F, O] = ???
+  def filter[F[_], O](s: Stream[F, O])(f: O => Boolean): Stream[F, O] = {
+    def go(s: Stream[F, O]): Pull[F, O, Unit] = {
+      s.pull.uncons.flatMap[F, O, Unit] {
+        case None           => Pull.done
+        case Some((hd, tl)) => Pull.output(hd.filter(f)) >> go(tl)
+      }
+    }
+    go(s).stream
+  }
 }

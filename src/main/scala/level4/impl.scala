61948af97f0b6e9b4bd82a0aac25162b905bc10b
Arul Madhavan <arulselvan1234@gmail.com>
2018-12-26 20:35:10 -0800

    Finished L4

--- src/main/scala/level4/impl.scala
+++ src/main/scala/level4/impl.scala
@@ -9,20 +9,24 @@ object impl {
     *
     * Implement me and others!!!
     */
-  def repeat[F[_], O](s: Stream[F, O]): Stream[F, O] = ???
+  def repeat[F[_], O](s: Stream[F, O]): Stream[F, O] = s ++ repeat(s)
 
   /**
     * Strips all output from a stream
     */
-  def drain[F[_], O](s: Stream[F, O]): Stream[F, INothing] = ???
+  def drain[F[_], O](s: Stream[F, O]): Stream[F, INothing] = s >> Stream.empty
 
   /**
     * Runs an effect and ignores its output
     */
-  def eval_[F[_], A](fa: F[A]): Stream[F, INothing] = ???
+  def eval_[F[_], A](fa: F[A]): Stream[F, INothing] =
+    Stream.eval(fa) >> Stream.empty
 
   /**
     * Catches any errors produced by a stream
     */
-  def attempt[F[_], O](s: Stream[F, O]): Stream[F, Either[Throwable, O]] = ???
+  def attempt[F[_], O](s: Stream[F, O]): Stream[F, Either[Throwable, O]] =
+    s.map(Right(_)).handleErrorWith {
+      case t: Throwable => Stream.emit(Left[Throwable, O](t))
+    }
 }
